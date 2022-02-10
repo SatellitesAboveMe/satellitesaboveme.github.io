9eea5231e51c4097a4f6f5febbd7f10b49ab309d
Andrei Kechahmadze <a.kechahmadze@itransition.com>
2022-02-10 12:49:47 +0300

    refactor: required changes

--- src/Components/Main/index.tsx
+++ src/Components/Main/index.tsx
@@ -1,12 +1,14 @@
 import { Container } from '@mui/material'
-import { LocationForm } from 'components/LocationForm'
-import 'components/Main/Main.scss'
+import { LocationForm } from 'components/locationForm'
+import 'components/main/index.scss'
+import { SatelliteTable } from 'components/satellitesTable'
 
 export const Main = () => {
   return (
     <Container fixed>
         <h1>Input your location</h1>
         <LocationForm />
+        <SatelliteTable />
     </Container>
   )
 }
