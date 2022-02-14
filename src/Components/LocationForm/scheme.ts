7c7eddf845a4dd50f755b2429f68ce1464c33613
Andrei <99184752+akechahmadze@users.noreply.github.com>
2022-02-14 15:14:23 +0300

    Delete src/Components directory

--- src/Components/LocationForm/scheme.ts
+++
@@ -1,8 +0,0 @@
-import { object, number } from 'yup'
-
-export const scheme = object({
-  latitude: number().required().min(-90).max(90),
-  longitude: number().required().min(-180).max(180),
-  radius: number().required().min(0).max(90),
-  category: number().required().min(1).max(5) // currently only available from 1 to 5
-})
