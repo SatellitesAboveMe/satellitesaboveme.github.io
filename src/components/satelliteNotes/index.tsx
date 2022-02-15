4e45cd2b53c59424352014d48e02f6dfdf848efb
Andrei Kechahmadze <a.kechahmadze@itransition.com>
2022-02-15 15:21:54 +0300

    refactor: required changes

--- src/components/satelliteNotes/index.tsx
+++ src/components/satelliteNotes/index.tsx
@@ -5,7 +5,7 @@ import { Note } from 'components/satelliteNotes/note'
 import { Container, Grid } from '@mui/material'
 
 interface SatelliteNotesProps {
-    id: number
+  id: number
 }
 
 const SatelliteNotesComponent = observer((props: SatelliteNotesProps) => {
@@ -17,27 +17,27 @@ const SatelliteNotesComponent = observer((props: SatelliteNotesProps) => {
 
   return (
     <>
-    <h2>Notes:</h2>
-    {
-    notes
-      ? (
-    <Grid container spacing={2}>
-      {notes.map((note, index) => <Note key={index} {...note} />)}
-    </Grid>
-        )
-      : <span>There are no notes</span>
-    }
-    <Container maxWidth={'xs'}>
-    <NotesForm id={id}/>
-    </Container>
+      <h2>Notes:</h2>
+      {
+        notes
+          ? (
+            <Grid container spacing={2}>
+              {notes.map((note, index) => <Note key={index} {...note} />)}
+            </Grid>
+            )
+          : <span>There are no notes</span>
+      }
+      <Container maxWidth={'xs'}>
+        <NotesForm id={id} />
+      </Container>
     </>
   )
 })
 
 const SatelliteNotes = (props: SatelliteNotesProps) => (
-    <SatelliteNotesStoreContext.Provider value={satelliteNotesStore}>
-        <SatelliteNotesComponent id={props.id} />
-    </SatelliteNotesStoreContext.Provider>
+  <SatelliteNotesStoreContext.Provider value={satelliteNotesStore}>
+    <SatelliteNotesComponent id={props.id} />
+  </SatelliteNotesStoreContext.Provider>
 )
 
 export default SatelliteNotes
