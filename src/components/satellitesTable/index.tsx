a530340cedfba77f03850fa914a67a5fd9d7756f
Andrei Kechahmadze <a.kechahmadze@itransition.com>
2022-02-25 16:41:04 +0300

    feature: satellites datagrid

--- src/components/satellitesTable/index.tsx
+++ src/components/satellitesTable/index.tsx
@@ -1,18 +1,24 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { satelliteTableStore, SatelliteTableStoreContext, useSatelliteTableStore } from 'stores/satelliteTableStore'
 import { observer } from 'mobx-react-lite'
-import Table from '@mui/material/Table'
-import TableBody from '@mui/material/TableBody'
-import TableCell from '@mui/material/TableCell'
-import TableContainer from '@mui/material/TableContainer'
-import TableHead from '@mui/material/TableHead'
-import TableRow from '@mui/material/TableRow'
-import Paper from '@mui/material/Paper'
+import { DataGrid } from '@mui/x-data-grid'
 import { makeStyles } from '@mui/styles'
 import { SingleSatelliteData } from 'api/above'
 import { useNavigate } from 'react-router-dom'
 import { RenderDependingOnState } from 'components/renderDependingOnState'
+import { Box } from '@mui/material'
+import './index.scss'
 
-const TableComponent = ({ satelliteData }: {satelliteData: SingleSatelliteData[]}) => {
+const columns = [
+  { field: 'id', headerName: 'Id', type: 'number', flex: 1 },
+  { field: 'satname', headerName: 'Name', flex: 1 },
+  { field: 'launchDate', headerName: 'Launch Date', type: 'date', flex: 1 },
+  { field: 'satlat', headerName: 'Latitude', type: 'number', flex: 1 },
+  { field: 'satlng', headerName: 'Longitude', type: 'number', flex: 1 },
+  { field: 'satalt', headerName: 'Altitude', type: 'number', flex: 1 }
+]
+
+const TableComponent = ({ satelliteData }: { satelliteData: SingleSatelliteData[] }) => {
   const useStyles = makeStyles(() => ({
     hover: {
       '&:hover': {
@@ -21,40 +27,18 @@ const TableComponent = ({ satelliteData }: {satelliteData: SingleSatelliteData[]
     }
   }))
 
-  const navigate = useNavigate()
+  const rows = satelliteData.map(
+    ({ satid, launchDate, ...others }) => ({
+      id: satid,
+      launchDate: new Date(launchDate),
+      ...others
+    })
+  )
 
-  const classes = useStyles()
   return (
-    <TableContainer component={Paper}>
-      <Table aria-label="satellites table">
-        <TableHead>
-          <TableRow>
-            <TableCell align="right">Id</TableCell>
-            <TableCell align="right">Name</TableCell>
-            <TableCell align="right">Launch Date</TableCell>
-            <TableCell align="right">Latitude</TableCell>
-            <TableCell align="right">Longitude</TableCell>
-            <TableCell align="right">Altitude</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {
-            satelliteData.map(
-              satellite => (
-                <TableRow hover classes={{ hover: classes.hover }} key={satellite.satid} onClick={() => navigate(`satelliteInfo/${satellite.satid}`)}>
-                  <TableCell align="right">{satellite.satid}</TableCell>
-                    <TableCell align="right">{satellite.satname}</TableCell>
-                    <TableCell align="right">{satellite.launchDate}</TableCell>
-                    <TableCell align="right">{satellite.satlat}</TableCell>
-                    <TableCell align="right">{satellite.satlng}</TableCell>
-                    <TableCell align="right">{satellite.satalt}</TableCell>
-                  </TableRow>
-              )
-            )
-          }
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <Box mb={3}>
+      <DataGrid columns={columns} rows={rows} autoHeight onRowClick={({ id }) => window.open(`/satelliteInfo/${id}`, '_blank')} getRowClassName={() => 'datagrid-row'} />
+    </Box>
   )
 }
 
@@ -64,13 +48,13 @@ const SatelliteTableComponent = observer(() => {
   const { satelliteData, state } = table
 
   return <RenderDependingOnState
-  state={state}
-  InfoComponent={<TableComponent satelliteData={satelliteData}/>}
+    state={state}
+    InfoComponent={<TableComponent satelliteData={satelliteData} />}
   />
 })
 
 export const SatellitesTable = () => (
-    <SatelliteTableStoreContext.Provider value={satelliteTableStore}>
-        <SatelliteTableComponent />
-    </SatelliteTableStoreContext.Provider>
+  <SatelliteTableStoreContext.Provider value={satelliteTableStore}>
+    <SatelliteTableComponent />
+  </SatelliteTableStoreContext.Provider>
 )
